6e94b6d1dba1ed8d9cd2ef00d71f2a71ebabf6d0
shubham agarwal <shubham@shubhams-Laptop.local>
2025-06-19 10:42:44 +0530

    Reset branch with new Python-Java integration app

--- src/main/resources/scripts/GUARDRAIL-3/crew_agents/kyu_agent.py
+++
@@ -1,9 +0,0 @@
-# crew_agents/kyu_agent.py
-from kyu_module import predict_kyu_score
-
-class KYUAgent:
-    def __init__(self, name="KYU Agent"):
-        self.name = name
-
-    def run(self, email, domain, purpose):
-        return predict_kyu_score(email, domain, purpose)
